2c944f57b0fdd8e7d640388843a2bd09a99f0505
sonercirit <sonercirit@gmail.com>
2019-11-01 23:55:34 +0300

    adds data model and maps data to model

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,32 @@ import xml2js from "xml2js";
 import get from "./helpers/request";
 import { EARTHQUAKE_ENDPOINT } from "./helpers/config";
 
+interface Earthquake {
+  date: Date;
+  location: string;
+  latitude: number;
+  longitude: number;
+  magnitude: number;
+  depth: number;
+}
+
 async function bootstrap(): Promise<void> {
   const data = await get(EARTHQUAKE_ENDPOINT);
   const parsedData = await xml2js.parseStringPromise(data);
-  console.log(data, parsedData);
+  const mappedData = parsedData.eqlist.earhquake.map(
+    (x: { $: Record<string, string> }): Earthquake => {
+      const { $ } = x;
+      return {
+        date: new Date($.name.replace(".", "-")),
+        location: $.lokasyon.trim(),
+        latitude: Number($.lat),
+        longitude: Number($.lng),
+        magnitude: Number($.mag),
+        depth: Number($.Depth)
+      };
+    }
+  );
+  console.log(mappedData);
 }
 
 bootstrap();
