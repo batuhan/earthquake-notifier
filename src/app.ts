1261e738bedc50a07d18c05533ea7682e6d018bc
sonercirit <sonercirit@gmail.com>
2019-11-02 01:17:28 +0300

    implements geocode reverse lookup

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,34 @@ import xml2js from "xml2js";
 import { createConnection } from "typeorm";
 import get from "./helpers/request";
 import { EARTHQUAKE_ENDPOINT } from "./helpers/config";
-import Earthquake from "./entities/earthquake";
+import Earthquake, { Geocodes } from "./entities/earthquake";
+import { openStreetMapGeocoder } from "./helpers/geocode";
 
 async function save($: Record<string, string>): Promise<Earthquake | null> {
   const date = new Date($.name.replace(".", "-"));
   const location = $.lokasyon.trim();
   const oldEarthquake = await Earthquake.findOne({ where: { date, location } });
   if (!oldEarthquake) {
-    const newEarthquake = new Earthquake();
-    newEarthquake.date = date;
-    newEarthquake.location = location;
-    newEarthquake.latitude = Number($.lat);
-    newEarthquake.longitude = Number($.lng);
-    newEarthquake.magnitude = Number($.mag);
-    newEarthquake.depth = Number($.Depth);
-    return newEarthquake.save();
+    const earthquake = new Earthquake();
+    earthquake.date = date;
+    earthquake.location = location;
+    earthquake.latitude = Number($.lat);
+    earthquake.longitude = Number($.lng);
+    earthquake.magnitude = Number($.mag);
+    earthquake.depth = Number($.Depth);
+    const geocodes: Geocodes = {};
+    const loc = {
+      lat: earthquake.latitude,
+      lon: earthquake.longitude
+    };
+    try {
+      geocodes.openStreetMap = await openStreetMapGeocoder.reverse(loc);
+      // geocodes.google = await googleGeocoder.reverse(loc);
+    } catch (e) {
+      console.log(e);
+    }
+    earthquake.geocodes = geocodes;
+    return earthquake.save();
   }
   return null;
 }
